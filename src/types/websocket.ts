08e162149428b342bc72884bb14d52c63f0bf57f
chenyunwen <sssh10110@gmail.com>
2025-05-28 18:40:06 +0800

    feat: add websocket

---
+++ src/types/websocket.ts
@@ -0,0 +1,26 @@
+export type Vector3 = {
+  x: number;
+  y: number;
+  z: number;
+};
+
+export interface Player {
+  device_id: string;
+  chapter: number;
+  sequence: number;
+  ready_to_move: boolean;
+  left_hand_position: Vector3;
+  left_hand_forward: Vector3;
+  right_hand_position: Vector3;
+  right_hand_forward: Vector3;
+  left_hand_available: boolean;
+  right_hand_available: boolean;
+  head_position: Vector3;
+  head_forward: Vector3;
+}
+
+export interface WebSocketData {
+  room_id: string;
+  players: Player[];
+  player_count: number;
+}
